4ad87d8aba01a6bd0d0dcad24985ce032222ab7a
kim <ksc0407@khu.ac.kr>
2021-04-08 17:59:09 +0900

    C++로 멜로디를 만들고 화음 넣기

--- wave double array/사랑을 했다 악보.cpp
+++ wave double array/사랑을 했다 악보.cpp
@@ -24,7 +24,7 @@ int main()
 
     //Sprint 2
     *sampleRate = 8000;
-    *NumChannels = 1;
+    *NumChannels = 2;
 
     int C4 = 262; //도 (4옥타브)
     int D4 = 294; //레
@@ -37,7 +37,7 @@ int main()
 
 
      // Open your own file
-    ofstream myTract("사랑을 했다.wav", ios::binary | ios::out);
+    ofstream myTract("사랑을 했다(화음).wav", ios::binary | ios::out);
     if (!myTract) return 666;  // 만일 파일이 열리지 않으면 프로그램을 끝낸다. 
     myTract.write(header, 44 * sizeof(char));
     float dt = 1. / 8000.0;
@@ -50,26 +50,36 @@ int main()
     미레도 미솔            미레도 미레~
     E4,D4,C4,E4,G4, Z0,    E4,D4,C4,E4,D4,    Z0,
     n, n, n, n, n*3,n / 2, n, n, n, n, n * 3, n / 2,
+    시라솔 시레            시라솔 시라
+    B4,A4,G4,B4,2*D4, Z0,    B4,A4,G4,B4,A4,    Z0,
 
     지우지 못할          추억이 됐다~
     미레도 레레          도레도 미도~ @
     E4,D4,C4,D4,D4,      C4,D4,C4, E4,C4,    Z0,
     n, n, n, n, n * 2,   n, n, n,  n, n * 2, n / 2,
+    시라솔 라라          솔라솔 시솔
+    B4,A4,G4,A4,A4,      G4,A4,G4, B4,G4,    Z0,
 
     볼만한      멜로    드라마 ~
     미레도      레도    미미솔  @ 
     E4,D4,C4,   D4,C4,  E4,E4,G4, Z0, 
     n, n, n     n, n,   n, n, n * 3, n / 2,
+    시라솔      라솔    시레(높은 음)~
+    B4,A4,G4,   A4,G4,  B4,B4,2*D4, Z0,
 
     괜찮은      결말 ~
     미레도      미레 @ 
     E4,D4,C4,   E4,D4,Z0,
     n, n, n,    n, n*3, n/2,
+    시라솔      시라
+    B4,A4,G4,   B4,A4,Z0,
 
     그거면     됐다   널   사랑했다~
     미레도     레레   도   레도미도~ @ 
     E4,D4,C4,  D4,D4, C4,  D4,C4,E4,C4,  Z0
     n, n, n,   n, n,  n,   n, n, n, n*2, n/2
+    시라솔     라라   솔   라솔시솔
+    B4,A4,G4,  A4,A4, G4,  A4,G4,B4,G4,  Z0
     */ 
     int order[49] = {
     E4,D4,C4,E4,G4, Z0,    E4,D4,C4,E4,D4,    Z0, //12
@@ -78,28 +88,37 @@ int main()
     E4,D4,C4,   E4,D4,Z0,                         //6
     E4,D4,C4,  D4,D4, C4,  D4,C4,E4,C4,  Z0       //11
     }; //C도 D레 E미 F파 G솔 A라 B시 Z쉼표
+    int order2[49] = {
+    B4,A4,G4,B4,2*D4, Z0,    B4,A4,G4,B4,A4,    Z0,
+    B4,A4,G4,A4,A4,      G4,A4,G4, B4,G4,    Z0,
+    B4,A4,G4,   A4,G4,  B4,B4,2 * D4, Z0,
+    B4,A4,G4,   B4,A4,Z0,
+    B4,A4,G4,  A4,A4, G4,  A4,G4,B4,G4,  Z0
+    };
     int nArray[49] = {
     n, n, n, n, n * 3,n / 2, n, n, n, n, n * 3, n / 2, //12
     n, n, n, n, n * 2,   n, n, n,  n, n * 2, n / 2,    //11 23
     n, n, n,     n, n,   n, n, n * 3, n / 2,           //9  32
     n, n, n,    n, n * 3, n / 2,                       //6  38
     n, n, n,   n, n,  n,   n, n, n, n * 2, n / 2       //11 49
-    };
+    }; // n=팔분음표
 
     int test = 49;
     short** music = new short* [test];
     int i, j;
     for (i = 0; i < test; i++) {
-        music[i] = new short[nArray[i]];
+        music[i] = new short[nArray[i]*2];
     }
     for (int i = 0; i < test; i++) {
-        for (int j = 0; j < nArray[i]; j++) {
-            music[i][j] = (short)(a * sin(2.0 * pi * order[i] * j * dt));
+        for (int j = 0; j < 2*nArray[i]; j++) {
+            if (j % 2 == 0) { music[i][j] = (short)(a * sin(2.0 * pi * order[i]/2 * j * dt)); }
+            else { music[i][j] = (short)(a * sin(2.0 * pi * order2[i]/2 * j * dt)); }
+
         }
 
     }
     for (i = 0; i < test; i++) {
-        myTract.write((char*)music[i], sizeof(char) * nArray[i]);
+        myTract.write((char*)music[i], sizeof(char) * nArray[i]*2);
 
     }
 
