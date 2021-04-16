e431c0b7c32e19284779beb7d1d7dd5278684370
kim <ksc0407@khu.ac.kr>
2021-04-16 20:01:10 +0900

    저장 완료

--- wave double array/wave double array.cpp
+++ wave double array/wave double array.cpp
@@ -7,27 +7,25 @@ const float pi = 3.141592;
 
 int main()
 {
-  
+
     char header[44];
-    short data[4000];
+    short data[50];
     //Sprint 1 
     //read binary file
     ifstream head("Beatles-LetItBe-wav_30s.wav", ios::binary | ios::in);
     if (!head) return 999;
     head.read(header, sizeof(header));
 
-    head.read((char*)data, sizeof(short) * 4000);
-    
+    head.read((char*)data, sizeof(short) * 50);
+    head.close();
     short* NumChannels = (short*)(header + 22); cout << "NumChannels : " << NumChannels << endl;
     int* sampleRate = (int*)(header + 24); cout << "sampling rate : " << sampleRate << endl;
     short* BitsPerSample = (short*)(header + 34); cout << "BitsPerSample: " << BitsPerSample << endl;
 
     //Sprint 2
     *sampleRate = 8000;
-    *NumChannels = 1;
-    *BitsPerSample = 16;
+    *NumChannels = 2;
 
-    head.close();
     int C4 = 262; //도 (4옥타브)
     int D4 = 294; //레
     int E4 = 330; //미
@@ -37,91 +35,98 @@ int main()
     int B4 = 494; //시
     int Z0 = 0; //쉼표  왜 define으로 하면 함수의 매개변수로 못받지?
 
-  
+
      // Open your own file
-    ofstream myTract("사랑을 했다.wav", ios::binary | ios::out);
+    ofstream myTract("사랑을 했다(화음).wav", ios::binary | ios::out);
     if (!myTract) return 666;  // 만일 파일이 열리지 않으면 프로그램을 끝낸다. 
     myTract.write(header, 44 * sizeof(char));
     float dt = 1. / 8000.0;
-    int n = 4000;  float a = 10000.0, f = 440.0;// 라
-    int order[15] = { E4,D4,C4,E4,G4,Z0,E4,D4,C4,E4,D4,Z0,E4,D4,C4 };
-    int nArray[15] = { n,n,n,n,n,n*3,n/2,n,n,n,n*3,n/2,n,n,n };
-    
-    
+    int n = 4000;
+    float a = 20000.0, f = 440.0;// 라
 
-   
-    short** music = new short* [15];
+    //15 + 19 + 19 + 22 + 18 93개 음
+    /*
+    사랑을 했다            우리가 만나~
+    미레도 미솔            미레도 미레~
+    E4,D4,C4,E4,G4, Z0,    E4,D4,C4,E4,D4,    Z0,
+    n, n, n, n, n*3,n / 2, n, n, n, n, n * 3, n / 2,
+    시라솔 시레            시라솔 시라
+    B4,A4,G4,B4,2*D4, Z0,    B4,A4,G4,B4,A4,    Z0,
+
+    지우지 못할          추억이 됐다~
+    미레도 레레          도레도 미도~ @
+    E4,D4,C4,D4,D4,      C4,D4,C4, E4,C4,    Z0,
+    n, n, n, n, n * 2,   n, n, n,  n, n * 2, n / 2,
+    시라솔 라라          솔라솔 시솔
+    B4,A4,G4,A4,A4,      G4,A4,G4, B4,G4,    Z0,
+
+    볼만한      멜로    드라마 ~
+    미레도      레도    미미솔  @
+    E4,D4,C4,   D4,C4,  E4,E4,G4, Z0,
+    n, n, n     n, n,   n, n, n * 3, n / 2,
+    시라솔      라솔    시레(높은 음)~
+    B4,A4,G4,   A4,G4,  B4,B4,2*D4, Z0,
+
+    괜찮은      결말 ~
+    미레도      미레 @
+    E4,D4,C4,   E4,D4,Z0,
+    n, n, n,    n, n*3, n/2,
+    시라솔      시라
+    B4,A4,G4,   B4,A4,Z0,
+
+    그거면     됐다   널   사랑했다~
+    미레도     레레   도   레도미도~ @
+    E4,D4,C4,  D4,D4, C4,  D4,C4,E4,C4,  Z0
+    n, n, n,   n, n,  n,   n, n, n, n*2, n/2
+    시라솔     라라   솔   라솔시솔
+    B4,A4,G4,  A4,A4, G4,  A4,G4,B4,G4,  Z0
+    */
+    int order[49] = {
+    E4,D4,C4,E4,G4, Z0,    E4,D4,C4,E4,D4,    Z0, //12
+    E4,D4,C4,D4,D4,      C4,D4,C4, E4,C4,    Z0,  //11
+    E4,D4,C4,   D4,C4,  E4,E4,G4, Z0,             //9
+    E4,D4,C4,   E4,D4,Z0,                         //6
+    E4,D4,C4,  D4,D4, C4,  D4,C4,E4,C4,  Z0       //11
+    }; //C도 D레 E미 F파 G솔 A라 B시 Z쉼표
+    int order2[49] = {
+    B4,A4,G4,B4,2 * D4, Z0,    B4,A4,G4,B4,A4,    Z0,
+    B4,A4,G4,A4,A4,      G4,A4,G4, B4,G4,    Z0,
+    B4,A4,G4,   A4,G4,  B4,B4,2 * D4, Z0,
+    B4,A4,G4,   B4,A4,Z0,
+    B4,A4,G4,  A4,A4, G4,  A4,G4,B4,G4,  Z0
+    };
+    int nArray[49] = {
+    n, n, n, n, n * 3,n / 2, n, n, n, n, n * 3, n / 2, //12
+    n, n, n, n, n * 2,   n, n, n,  n, n * 2, n / 2,    //11 23
+    n, n, n,     n, n,   n, n, n * 3, n / 2,           //9  32
+    n, n, n,    n, n * 3, n / 2,                       //6  38
+    n, n, n,   n, n,  n,   n, n, n, n * 2, n / 2       //11 49
+    }; // n=팔분음표
+
+    int test = 49;
+    short** music = new short* [test];
     int i, j;
-    for (i = 0; i < 15; i++) {
-        music[i] = new short[4000];
+    for (i = 0; i < test; i++) {
+        music[i] = new short[nArray[i] * 2];
     }
-    for (int i = 0; i < 15; i++) {
-        for (int j = 0; j < nArray[i]; j++) {
-            music[i][j] = (short)(a * sin(2.0 * pi * order[j] * j * dt));
+    for (int i = 0; i < test; i++) {
+        for (int j = 0; j < 2 * nArray[i]; j++) {
+            if (j % 2 == 0) { music[i][j] = (short)(a * sin(2.0 * pi * order[i] / 2 * j * dt)); }
+            else { music[i][j] = (short)(a * sin(2.0 * pi * order2[i] / 2 * j * dt)); }
 
         }
 
     }
-    for (i = 0; i < 15; i++) {
-        myTract.write((char*)music[i], sizeof(char) * 4000);
+    for (i = 0; i < test; i++) {
+        myTract.write((char*)music[i], sizeof(char) * nArray[i] * 2);
 
     }
+
     myTract.close();
-    //myTract.write((char*)data1, n * sizeof(char));  myTract.write((char*)music[i][j], nArray[i] * sizeof(char));
-    
-    //#define C4 = 262 //도 (4옥타브)
-    //#define D4 = 294 //레
-    //#define E4 = 330 //미
-    //#define F4 = 349 //파
-    //#define G4 = 392 //솔
-    //#define A4 = 440 //라
-    //#define B4 = 494 //시
-     //#define Z0 = 0 //쉼표
-    
-   // for (int i = 0; i < 15; i++) {
-   //    wave(music[i][0], n, a, E4); //n = 44100, a = 30000.0, f = 440.0;// 라
-   //    
-   //}
-
-   //15 + 19 + 19 + 22 + 18 93개 음 표현할 예정
-    //int order[93] = { E4,D4,C4,E4,G4,Z0,E4,D4,C4,E4,D4,Z0,E4,D4,C4 };
-    /* --------진폭 그대로, 주파수(계이름)변경, n은 1박 4000(0.5초) --------- */
-    //for (int i = 0; i < 15; i++) {
-    //    //wave(music[i][0], n, a, E4); //n = 44100, a = 30000.0, f = 440.0;// 라
-    //    cout << " music   " << music[i][0] << endl;
-    //}
-
-    //myTract.write((char*)data1, n * sizeof(char));
-    //myTract.write((char*)data2, n * sizeof(char));
-    //myTract.write((char*)data3, n * sizeof(char));
-    //myTract.write((char*)data4, n * sizeof(char));
-    //myTract.write((char*)data5, n * 3*sizeof(char));
-    //myTract.write((char*)data6, n/2 * sizeof(char));
-    //myTract.write((char*)data7, n * sizeof(char));
-    //myTract.write((char*)data8, n * sizeof(char));
-    //myTract.write((char*)data9, n * sizeof(char));
-    //myTract.write((char*)data10, n * sizeof(char));
-    //myTract.write((char*)data11, n *3* sizeof(char));
-    //myTract.write((char*)data12, n /2* sizeof(char));
-    //myTract.write((char*)data13, n * sizeof(char));
-    //myTract.write((char*)data14, n * sizeof(char));
-    //myTract.write((char*)data15, n * sizeof(char));
-
-    
-    for (i = 0; i < 15; i++)
+    for (i = 0; i < test; i++)
         free(music[i]);
     free(music); //메모리 해제
-    //myTract.close();
+
 
     return 1;
-}
-
-void wave(short* pArray, int n, int a, int f) {
-    float dt = 1. / 8000.0;
-    for (int j = 0; j < 15; j++) {
-        for (int i = 0; i < n; i++) {
-            pArray[j][&i] = (short)(a * sin(2.0 * pi * f * i * dt));
-
-        }
-    }
 }
